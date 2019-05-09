d62c0a9bc9f6e53896b725ee07a23ad085af7354
Shane Avery Sistoza <ss2010@hawaii.edu>
2019-05-09 09:39:36 -0500

    begin writing unit tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
 import App from './App';
+import configureStore from './redux';
+import Firebase, { FirebaseContext } from './firebase';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let store;
+  let wrapper;
+  beforeEach(() => {
+    store = configureStore();
+    wrapper = shallow(
+      <FirebaseContext.Provider value={Firebase}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </FirebaseContext.Provider>
+    );
+  });
+
+  it('should not crash', () => {
+    shallow(<App />);
+  });
 });
